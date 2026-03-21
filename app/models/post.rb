6d9d6953f3decd691db83f0f468c17e61cd89f20
Rinrada Wattanachanasorn <rinradawattanachanasorn@Rinradas-MacBook-Air.local>
2026-03-21 16:35:30 +0700

    add: change for lesson4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
     belongs_to :user
+    validate :cannot_edit_once_published
+
+private
+def cannot_edit_once_published
+  if published? && changed? && !published_changed?
+    errors.add(:base, "Published posts cannot be edited")
+  end
+end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create!(email: "test_1@test.com", password: "password123")
+  end
+
+  test "draft post can be updated" do
+    post = Post.create!(title: "Draft", body: "content", published: false, user: @user)
+    post.update!(title: "Updated Draft")
+    assert_equal "Updated Draft", post.title
+  end
+
+  test "published post cannot be edited" do
+    post = Post.create!(title: "Truth", body: "content", published: true, user: @user)
+    post.title = "Edited Truth"
+    assert_not post.valid?
+    assert_includes post.errors[:base], "Published posts cannot be edited"
+  end
 end
